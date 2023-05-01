ac4295855534f324f2457bc4ffc5b12504915aad
Rolem Galvez <rolemgalvez@gmail.com>
2023-05-01 14:54:25 +0800

    Add project files.

---
+++ HomeworkTwentyLibrary/Process.cs
@@ -0,0 +1,18 @@
+﻿using HomeworkTwentyLibrary.Models;
+
+namespace HomeworkTwentyLibrary
+{
+    public static class Process
+    {
+        public static void Assign(PersonModel person)
+        {
+            person.FirstName = "Juan";
+            person.LastName = "Cruz";
+        }
+
+        public static void Display(PersonModel person)
+        {
+            Console.WriteLine($"{person.FirstName} {person.LastName}");
+        }
+    }
+}
---
+++ HomeworkTwentyUI/Program.cs
@@ -0,0 +1,8 @@
+﻿using HomeworkTwentyLibrary;
+using HomeworkTwentyLibrary.Models;
+
+PersonModel person = new PersonModel();
+Process.Assign(person);
+Process.Display(person);
+
+Console.ReadLine();
